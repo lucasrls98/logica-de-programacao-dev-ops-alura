4d061f138d3b25130b69a5fdb5b3b17234f1f98b
lucasrls98 <lucasrls98@gmail.com>
2025-10-04 11:15:11 -0300

    Remove unused script reference and add while loop activity

---
+++ while-activity.js
@@ -0,0 +1,36 @@
+// 1 - Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// let contador = 1
+
+// while(contador <= 10){
+//     console.log(contador)
+//     contador ++
+// }
+
+// 2-Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// let contador = 10
+
+// while(contador >= 0){
+//     console.log(contador)
+//     contador --
+// }
+
+// 3.Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+// let contagemRegressiva = prompt('Você gostaria de começar a contagem regressiva a partir de qual número?')
+
+// while(contagemRegressiva >= 0){
+//     console.log(contagemRegressiva)
+//     contagemRegressiva -- 
+// }
+
+// 4.Crie um programa de contagem progressiva. Peça um número ao usuario e conte de 0 até esté número, usando um loop while no console do navegador.
+
+// let contagemProgressiva = prompt('Até qual número você gostaria de fazer a contagem?')
+// let contador = 0;
+
+// while(contador <= contagemProgressiva){
+//     console.log(contador)
+//     contador ++
+// }
