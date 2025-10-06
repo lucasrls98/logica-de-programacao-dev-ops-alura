2568427dea29078bdc41e4e9651e09989fd21a25
lucasrls98 <lucasrls98@gmail.com>
2025-10-06 19:57:19 -0300

    Solving a code challenge about random numbers

---
+++ random-number-activity.js
@@ -0,0 +1,85 @@
+//1-Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+// console.log('Seja bem vindo!')
+
+// 2-Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+
+// let nome = 'Lucas'
+// console.log(`Olá, ${nome}`)
+
+// 3.Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+// let nome2 = 'Lucas'
+// alert(`Olá, ${nome2}`)
+
+// 4.Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+// let linguagemPreferida = prompt('Qual é a linguagem que você mais gosta? ')
+
+// console.log(linguagemPreferida)
+
+// 5-Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+// let valor1 = 2
+// let valor2 = 6
+
+// resultado = valor1 + valor2
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`)
+
+// 6 - Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+// let valorUm = 5
+// let valorDois = 8
+
+// resultadoSubtracao = valor1 + valor2
+
+// console.log(`A diferença de ${valorUm} e ${valorDois} é igual a ${resultadoSubtracao}.`)
+
+// 7 - Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+
+// idade = prompt('Qual é sua idade?')
+
+// let mensagemIdade = idade >= 18 ? 'Você é maior de idade' : 'Você é menor de idade'
+
+// alert(mensagemIdade)
+
+// 8- Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+// numero = parseFloat(prompt('Insira um valor'))
+
+// let mensagemNumero = numero > 0
+//  ? 'O número é positivo!' 
+//  : numero === 0
+//     ? 'O número é zero'
+//     : 'O número é negativo'
+    
+// alert(mensagemNumero)
+
+// 9-Use um loop while para imprimir os números de 1 a 10 no console.
+
+// let numero  = 1
+// while(numero <= 10){
+//     console.log(numero)
+//     numero ++
+// }
+
+// 10-Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+// nota = 6
+// let mensagemAprovacao = nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+// alert(mensagemAprovacao)
+
+// 11-Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+// let qualquerNumero = Math.random()
+// console.log(qualquerNumero)
+
+// 12-Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+// let numeroDe1a10 = parseInt(Math.random() * 10 + 1)
+// console.log(numeroDe1a10)
+// 13-Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+// let numeroDe1a1000 = parseInt(Math.random() * 1000 + 1)
+// console.log(numeroDe1a1000)
+
